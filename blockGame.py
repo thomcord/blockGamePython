94057a5d2e3c140c3a7d45b8378671b5a478b0e2
thomcord <tc@MacBook-Pro.local>
2020-06-09 16:24:36 +0200

    created screen, set background color, created first rectangle and set color

--- blockGame.py
+++ blockGame.py
@@ -1,8 +1,44 @@
 import pygame 
+import sys
 
 pygame.init()
 
 width = 800
 height = 600
 
+red = (255,0,0)
+background_color = (0,0,0)
+
+player_size = 50
+player_pos= [width/2, height-2*player_size]
+
+
 screen = pygame.display.set_mode((width,height))
+
+game_over = False
+
+while not game_over:
+    
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            sys.exit()
+            
+        if event.type == pygame.KEYDOWN:
+            
+            x = player_pos[0]
+            y = player_pos[1]
+            
+            if event.key == pygame.K_LEFT:
+                x -= player_size
+            elif event.key == pygame.K_RIGHT:
+                x += player_size
+                
+            player_pos = [x,y]
+    
+    screen.fill((background_color))        
+    pygame.draw.rect(screen, red, (player_pos[0], player_pos[1], player_size, player_size))
+    
+    pygame.display.update()
+       
+        
