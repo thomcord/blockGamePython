cff5be249549647212abf655c37f93cd7af9dfea
thomcord <tc@MacBook-Pro.local>
2020-06-09 16:48:41 +0200

    created enemy, made it move from top to bottom of screen

--- blockGame.py
+++ blockGame.py
@@ -1,4 +1,5 @@
 import pygame 
+import random
 import sys
 
 pygame.init()
@@ -7,15 +8,22 @@ width = 800
 height = 600
 
 red = (255,0,0)
+blue = (0,0,255)
 background_color = (0,0,0)
 
 player_size = 50
 player_pos= [width/2, height-2*player_size]
 
+enemy_syze = 50
+enemy_pos = [random.randint(0, width-enemy_syze), 0]
+
+speed = 10
 
 screen = pygame.display.set_mode((width,height))
 
 game_over = False
+
+clock = pygame.time.Clock()
 
 while not game_over:
     
@@ -36,8 +44,27 @@ while not game_over:
                 
             player_pos = [x,y]
     
-    screen.fill((background_color))        
+    screen.fill(background_color)
+    
+    
+    #Updating position enemy
+    if enemy_pos[1] >= 0 and enemy_pos[1] < height:
+        enemy_pos[1] += speed
+    else:
+        enemy_pos[0] = random.randint(0, width-enemy_syze)
+        enemy_pos[1] = 0
+        
+    pygame.draw.rect(screen, blue, (enemy_pos[0], enemy_pos[1], enemy_syze, enemy_syze))        
     pygame.draw.rect(screen, red, (player_pos[0], player_pos[1], player_size, player_size))
+    
+    clock.tick(30)
+    
+    def detect_colision(player_pos, enemy_pos):
+        p_x = player_pos[0]
+        p_y = player_pos[1]
+        
+        e_x = enemy_pos[0]
+        e_x = enemy_pos[1]
     
     pygame.display.update()
        
