688790a1fe297b74266e717f5e03bdcb712a56c1
thomcord <tc@MacBook-Pro.fritz.box>
2020-06-15 14:27:14 +0200

    updating enemy code

--- blockGame.py
+++ blockGame.py
@@ -16,6 +16,7 @@ player_pos= [width/2, height-2*player_size]
 
 enemy_syze = 50
 enemy_pos = [random.randint(0, width-enemy_syze), 0]
+enemy_list = [enemy_pos]
 
 speed = 10
 
@@ -24,6 +25,42 @@ screen = pygame.display.set_mode((width,height))
 game_over = False
 
 clock = pygame.time.Clock()
+
+def drop_enemies(enemy_list):
+    if len(enemy_list) < 10:
+        x_pos = random.randint(0, width - enemy_syze)
+        y_pos = 0
+        enemy_list.append([x_pos, y_pos])
+        
+def draw_enemies(enemy_list):
+    for enemy_pos in enemy_list:
+        pygame.draw.rect(screen, blue, (enemy_pos[0], enemy_pos[1], enemy_syze, enemy_syze)) 
+        
+def update_enemy_positions(enemy_list):
+    for idx,enemy_pos in enumerate(enemy_list):
+        if enemy_pos[1] >= 0 and enemy_pos[1] < height:
+            enemy_pos[1] += speed
+    else:
+        enemy_list.pop(idx)
+    
+def collision_check(enemy_list, player_pos):
+    for enemy_pos in enemy_list:
+        if detect_colision(enemy_pos, player_pos):
+            return True
+    return False
+
+def detect_colision(player_pos, enemy_pos):
+    p_x = player_pos[0]
+    p_y = player_pos[1]
+        
+    e_x = enemy_pos[0]
+    e_y = enemy_pos[1]
+        
+    if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x + enemy_syze)):
+        if (e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y + enemy_syze)):
+            return True
+    return False
+         
 
 
 while not game_over:
@@ -49,26 +86,27 @@ while not game_over:
     
     
     #Updating position enemy
-    if enemy_pos[1] >= 0 and enemy_pos[1] < height:
-        enemy_pos[1] += speed
-    else:
-        enemy_pos[0] = random.randint(0, width-enemy_syze)
-        enemy_pos[1] = 0
-        
-    pygame.draw.rect(screen, blue, (enemy_pos[0], enemy_pos[1], enemy_syze, enemy_syze))        
+    #if enemy_pos[1] >= 0 and enemy_pos[1] < height:
+    #    enemy_pos[1] += speed
+    #else:
+    #    enemy_pos[0] = random.randint(0, width-enemy_syze)
+    #    enemy_pos[1] = 0
+    #    
+ 
+    drop_enemies(enemy_list)
+    update_enemy_positions(enemy_list) 
+    
+    if collision_check(enemy_list, player_pos):
+        game_over = True
+        break
+      
+    draw_enemies(enemy_list)
+    
     pygame.draw.rect(screen, red, (player_pos[0], player_pos[1], player_size, player_size))
     
     clock.tick(30)
     
-    def detect_colision(player_pos, enemy_pos):
-        p_x = player_pos[0]
-        p_y = player_pos[1]
-        
-        e_x = enemy_pos[0]
-        e_x = enemy_pos[1]
-        
-        if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x + enemy_syze))
-         
+ 
     
     pygame.display.update()
        
